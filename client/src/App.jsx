62de84db3e5ed10f4b55fe285a11f4d2c47286ea
linhpha <linhpha@net.chalmers.se>
2025-06-23 19:09:39 +0200

    #18 implement story page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
-import JourneyPage from "./pages/SecondPage"; //Journey page will be use for story page atm
+import StoryPage from "./pages/StoryPage"; //Journey page will be use for story page atm
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainPage />} />
-        <Route path="/story" element={<JourneyPage />} />
+        <Route path="/story" element={<StoryPage/>} />
       </Routes>
     </BrowserRouter>
   );
