111d323e71c4300edb4db2bfd88e9c80e6f0208b
linhpha <linhpha@net.chalmers.se>
2025-06-23 21:12:50 +0200

    #20 adjust the position of model

--- client/src/pages/StoryPage.jsx
+++ client/src/pages/StoryPage.jsx
@@ -277,7 +277,7 @@ const Timeline = () => {
               <Canvas camera={{ position: [0, 1.5, 6], fov: 40 }}>
                 <ambientLight intensity={0.5} />
                 <directionalLight position={[2, 2, 2]} />
-                <PCModel scale={1.5} rotation={[0, -Math.PI / 2, 0]} />
+                <PCModel scale={1.5} rotation={[0, -Math.PI / 2, 0]}   position={[0.5, 0, 0]}  />
                 <OrbitControls enableZoom={false} />
                 <Environment preset="sunset" />
               </Canvas>
